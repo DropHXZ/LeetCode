66dff28b8da1bc0d1bd85cb9897ea1916195f9e2
huxz <message_sky@126.com>
2018-08-20 15:52:37 +0800

    none

---
+++ app/src/main/java/com/huxz/leetcode/MainActivity.java
@@ -0,0 +1,13 @@
+package com.huxz.leetcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
