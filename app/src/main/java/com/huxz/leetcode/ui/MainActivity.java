1c2c42a12cc62cc05caa0a4f635c0518c646f357
huxz <message_sky@126.com>
2018-08-20 16:21:08 +0800

    自定义单项链表

--- app/src/main/java/com/huxz/leetcode/ui/MainActivity.java
+++ app/src/main/java/com/huxz/leetcode/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.huxz.leetcode;
+package com.huxz.leetcode.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.huxz.leetcode.R;
 
 public class MainActivity extends AppCompatActivity {
 
