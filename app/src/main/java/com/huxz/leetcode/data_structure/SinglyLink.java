51ed16e7e33650df5c5999f6435f177efb63f960
huxz <message_sky@126.com>
2018-12-26 14:08:08 +0800

    瞎G8改

---
+++ app/src/main/java/com/huxz/leetcode/data_structure/SinglyLink.java
@@ -0,0 +1,54 @@
+package com.huxz.leetcode.data_structure;
+
+/**
+ * create by huzx on 2018/8/20
+ * 功能：自定义单向链表
+ */
+public class SinglyLink {
+
+	private String[] strings;
+	private int size;
+
+	/**
+	 * Initialize your data structure here.
+	 */
+	public SinglyLink() {
+		strings = new String[100];
+	}
+
+	/**
+	 * Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+	 */
+	public int get(int index) {
+		return index;
+	}
+
+	/**
+	 * Add a node of value val before the first element of the linked list. After the insertion, the new node will be the first node of the linked list.
+	 */
+	public void addAtHead(int val) {
+
+	}
+
+	/**
+	 * Append a node of value val to the last element of the linked list.
+	 */
+	public void addAtTail(int val) {
+
+	}
+
+	/**
+	 * Add a node of value val before the index-th node in the linked list. If index equals to the length of linked list, the node will be appended to the end of linked list. If index is greater than the length, the node will not be inserted.
+	 */
+	public void addAtIndex(int index, int val) {
+
+	}
+
+	/**
+	 * Delete the index-th node in the linked list, if the index is valid.
+	 */
+	public void deleteAtIndex(int index) {
+
+	}
+
+}
--- app/src/main/java/com/huxz/leetcode/ui/MainActivity.java
+++ app/src/main/java/com/huxz/leetcode/ui/MainActivity.java
@@ -1,7 +1,11 @@
 package com.huxz.leetcode.ui;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.huxz.leetcode.R;
 
@@ -11,5 +15,8 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+			Toast.makeText(MainActivity.this,"success",Toast.LENGTH_LONG).show();
+		}
 	}
 }
